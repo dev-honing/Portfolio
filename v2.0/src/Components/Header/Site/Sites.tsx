73f52a5dd24fd320686eca54222e59ca4d270d5b
Ho Nyeong <dev.honing@gmail.com>
2024-01-29 17:49:49 +0900

    Feat: GitHub와 Blog 링크로 이동하는 기능 추가
    - Blog URL은 수정이 필요하다!

--- v2.0/src/Components/Header/Site/Sites.tsx
+++ v2.0/src/Components/Header/Site/Sites.tsx
@@ -1,7 +1,17 @@
-// src/Pages/Header/Sites/Sites.tsx
+// src/Components/Header/Site/Sites.tsx
+import React from 'react';
+
+import "./Sites.css"
 
 const Sites: React.FC = () => {
-  return <div className="sites">Blog GitHub</div>;
+  return (
+    <div className="sites">
+      <a href="http://github.com/dev-honing" className="site" target="_blank" rel="noopener noreferrer">
+        GitHub
+      </a>
+      {/* 나중에 링크 수정하기! */}<a href="https://dev-honing.github.io/CRA-TEST/" className="site" target="_blank" rel="noopener noreferrer">Blog</a> 
+    </div>
+  );
 };
 
 export default Sites;
