923cdd25dc6e3dd014569ed4297b2b144d9f5cc0
Ho Nyeong <dev.honing@gmail.com>
2024-01-29 14:54:54 +0900

    Feat: 구직 상태에 따른 색상 토글
    - Preparing이 true이면, 구직 중이므로 초록색
    - Preparing이 false이면, 구직 완료이므로 빨간색

--- v2.0/src/Pages/Header/HeaderPage.tsx
+++ v2.0/src/Pages/Header/HeaderPage.tsx
@@ -1,12 +1,15 @@
 // src/Pages/Header/HeaderPage.tsx
+
 import Status from "./Status/Status";
 import Introduce from "./Introduce/Introduce";
 import Sites from "./Site/Sites";
 
 const Header: React.FC = () => {
+  const Preparing = true; // true: 구직 중, false: 구직 완료
+
   return (
     <div id="header">
-      <Status />
+      <Status employ={Preparing} />
       <Introduce />
       <Sites />
     </div>
--- v2.0/src/Pages/Header/Status/Status.tsx
+++ v2.0/src/Pages/Header/Status/Status.tsx
@@ -1,7 +1,17 @@
 // src/Pages/Header/Status/Status.tsx
 
-const Status: React.FC = () => {
-  return <div className="status">구직 중입니다.</div>;
+import React from "react";
+import "./Status.css";
+
+interface StatusProps {
+  employ: boolean;
+}
+
+const Status: React.FC<StatusProps> = ({ employ }) => {
+  const statusText = employ ? "구직 중" : "구직 완료";
+  const statusClass = employ ? "green" : "red";
+
+  return <div className={`status ${statusClass}`}>{statusText}</div>;
 };
 
 export default Status;
