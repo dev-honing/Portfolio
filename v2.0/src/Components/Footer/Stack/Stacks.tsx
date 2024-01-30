a3b5a76be43c753e5ff5dea37ac7a8ad09d1d803
Ho Nyeong <dev.honing@gmail.com>
2024-01-30 17:24:00 +0900

    Feat: AWS의 이미지 URL 첨부

--- v2.0/src/Components/Footer/Stack/Stacks.tsx
+++ v2.0/src/Components/Footer/Stack/Stacks.tsx
@@ -1,7 +1,30 @@
-// src/Pages/Footer/Stacks/Stacks.tsx
+import React from "react";
 
 const Stacks: React.FC = () => {
-  return <div className="stacks"></div>;
+  return (
+    <div className="stacks">
+      AWS<img
+        src="https://s3.ap-northeast-2.amazonaws.com/my-icons-svg/SVG/AWS.svg"
+        alt="AWS"
+        style={{ width: "10rem", height: "10rem" }}
+      />
+      HTML<img
+        src="https://my-icons-svg.s3.ap-northeast-2.amazonaws.com/SVG/HTML.svg"
+        alt="AWS"
+        style={{ width: "10rem", height: "10rem" }}
+      />
+      CSS<img
+        src="https://my-icons-svg.s3.ap-northeast-2.amazonaws.com/SVG/CSS.svg"
+        alt="AWS"
+        style={{ width: "10rem", height: "10rem" }}
+      />
+      JavaScript<img
+        src="https://my-icons-svg.s3.ap-northeast-2.amazonaws.com/SVG/JavaScript.svg"
+        alt="AWS"
+        style={{ width: "10rem", height: "10rem" }}
+      />
+    </div>
+  );
 };
 
 export default Stacks;
