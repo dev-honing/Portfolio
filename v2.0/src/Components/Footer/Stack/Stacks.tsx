d8f197412e7a12218f654d38397c5d7a16bd5566
Ho Nyeong <dev.honing@gmail.com>
2024-01-30 18:22:01 +0900

    Feat: 이미지를 클릭해 각각의 URL로 이동하도록 라우팅

--- v2.0/src/Components/Footer/Stack/Stacks.tsx
+++ v2.0/src/Components/Footer/Stack/Stacks.tsx
@@ -1,28 +1,44 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 
 const Stacks: React.FC = () => {
   return (
     <div className="stacks">
-      AWS<img
-        src="https://s3.ap-northeast-2.amazonaws.com/my-icons-svg/SVG/AWS.svg"
-        alt="AWS"
-        style={{ width: "10rem", height: "10rem" }}
-      />
-      HTML<img
-        src="https://my-icons-svg.s3.ap-northeast-2.amazonaws.com/SVG/HTML.svg"
-        alt="AWS"
-        style={{ width: "10rem", height: "10rem" }}
-      />
-      CSS<img
-        src="https://my-icons-svg.s3.ap-northeast-2.amazonaws.com/SVG/CSS.svg"
-        alt="AWS"
-        style={{ width: "10rem", height: "10rem" }}
-      />
-      JavaScript<img
-        src="https://my-icons-svg.s3.ap-northeast-2.amazonaws.com/SVG/JavaScript.svg"
-        alt="AWS"
-        style={{ width: "10rem", height: "10rem" }}
-      />
+      <Link to="/aws">
+        AWS
+        <img
+          src="https://s3.ap-northeast-2.amazonaws.com/my-icons-svg/SVG/AWS.svg"
+          alt="AWS"
+          style={{ width: "10rem", height: "10rem", cursor: 'pointer' }}
+        />
+      </Link>
+
+      <Link to="/html">
+        HTML
+        <img
+          src="https://my-icons-svg.s3.ap-northeast-2.amazonaws.com/SVG/HTML.svg"
+          alt="HTML"
+          style={{ width: "10rem", height: "10rem", cursor: 'pointer' }}
+        />
+      </Link>
+
+      <Link to="/css">
+        CSS
+        <img
+          src="https://my-icons-svg.s3.ap-northeast-2.amazonaws.com/SVG/CSS.svg"
+          alt="CSS"
+          style={{ width: "10rem", height: "10rem", cursor: 'pointer' }}
+        />
+      </Link>
+
+      <Link to="/javascript">
+        JavaScript
+        <img
+          src="https://my-icons-svg.s3.ap-northeast-2.amazonaws.com/SVG/JavaScript.svg"
+          alt="JavaScript"
+          style={{ width: "10rem", height: "10rem", cursor: 'pointer' }}
+        />
+      </Link>
     </div>
   );
 };
