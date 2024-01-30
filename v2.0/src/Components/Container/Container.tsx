e3f0713290c8948815014269a74ce285d7504873
Ho Nyeong <dev.honing@gmail.com>
2024-01-30 11:29:10 +0900

    Feat: 버튼을 누르면 Growth 컴포넌트를 팝업
    - useState를 이용해 boolean을 토글하며 Growth 컴포넌트를 팝업

--- v2.0/src/Components/Container/Container.tsx
+++ v2.0/src/Components/Container/Container.tsx
@@ -1,10 +1,21 @@
 // src/Pages/Container/Container.tsx
+import React, { useState } from "react";
 import Growth from "./Growth/Growth";
 
 const Container: React.FC = () => {
+  // 팝업의 활성화 상태를 나타내는 변수
+  const [isPopupVisible, setPopupVisible] = useState(false);
+
+  // 버튼 클릭 시, 팝업 상태를 토글하는 함수
+  const handleButtonClick = () => {
+    // setPopupVisible 함수를 호출해 isPopupVisible 상태를 반전
+    setPopupVisible(!isPopupVisible);
+  };
+
   return (
     <div id="container">
-      <Growth />
+      <button onClick={handleButtonClick}>성장 로그</button>
+      {isPopupVisible && <Growth />}
     </div>
   );
 };
