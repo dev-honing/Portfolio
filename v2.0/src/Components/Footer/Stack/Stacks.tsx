532912050656e14cd5abaa58a582033a88a91380
Ho Nyeong <dev.honing@gmail.com>
2024-02-02 17:14:18 +0900

    Feat: 기술 스택 컴포넌트들을 리팩토링 및 렌더링 로직 작성

--- v2.0/src/Components/Footer/Stack/Stacks.tsx
+++ v2.0/src/Components/Footer/Stack/Stacks.tsx
@@ -1,44 +1,42 @@
-import React from "react";
-import { Link } from 'react-router-dom';
+// src/Components/Footer/Stack/Stacks.tsx
 
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Stacks.css";
+
+// 각 기술 스택의 props를 정의하는 인터페이스 정의
+interface StackItemProps {
+  stackName: string;
+}
+
+// 기술 스택을 나타내는 함수 컴포넌트 정의
+const StackItem: React.FC<StackItemProps> = ({ stackName }) => (
+  <Link to={`/${stackName.toLowerCase()}`}> {/* 라우팅 경로는 toLowerCase()를 이용해 소문자로 변경 */}
+    {/* 각 스택의 이미지 */}
+    <img
+      src={`https://s3.ap-northeast-2.amazonaws.com/my-icons-svg/SVG/${stackName}.svg`} // s3 버킷에 업로드한 파일명이 stackName과 동일하므로 바로 렌더링에도 반영됨(stackName.svg)
+      alt={stackName}
+      style={{ width: "5rem", height: "5rem", cursor: "pointer" }}
+    />
+  </Link>
+);
+
+// 기술 스택을 렌더링하는 함수 컴포넌트 정의
 const Stacks: React.FC = () => {
+  // 각 스택의 데이터
+  const stacksData: StackItemProps[] = [
+    { stackName: "AWS" },
+    { stackName: "HTML" },
+    { stackName: "CSS" },
+    { stackName: "JavaScript" },
+  ];
+
   return (
     <div className="stacks">
-      <Link to="/aws">
-        AWS
-        <img
-          src="https://s3.ap-northeast-2.amazonaws.com/my-icons-svg/SVG/AWS.svg"
-          alt="AWS"
-          style={{ width: "10rem", height: "10rem", cursor: 'pointer' }}
-        />
-      </Link>
-
-      <Link to="/html">
-        HTML
-        <img
-          src="https://my-icons-svg.s3.ap-northeast-2.amazonaws.com/SVG/HTML.svg"
-          alt="HTML"
-          style={{ width: "10rem", height: "10rem", cursor: 'pointer' }}
-        />
-      </Link>
-
-      <Link to="/css">
-        CSS
-        <img
-          src="https://my-icons-svg.s3.ap-northeast-2.amazonaws.com/SVG/CSS.svg"
-          alt="CSS"
-          style={{ width: "10rem", height: "10rem", cursor: 'pointer' }}
-        />
-      </Link>
-
-      <Link to="/javascript">
-        JavaScript
-        <img
-          src="https://my-icons-svg.s3.ap-northeast-2.amazonaws.com/SVG/JavaScript.svg"
-          alt="JavaScript"
-          style={{ width: "10rem", height: "10rem", cursor: 'pointer' }}
-        />
-      </Link>
+      {/* 스택 데이터를 매핑하여 각 항목을 렌더링 */}
+      {stacksData.map((stack, index) => (
+        <StackItem key={index} {...stack} />
+      ))}
     </div>
   );
 };
